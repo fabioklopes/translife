f8e73999bc3b60645c2ca8bf71c53e3a58aa6890
Fábio Klevinskas Lopes <fabioklopes@live.com>
2025-06-28 11:16:26 -0300

    feature: Add Pickup entity

---
+++ src/main/java/com/translife/entity/Pickup.java
@@ -0,0 +1,28 @@
+package com.translife.entity;
+
+import java.time.LocalDateTime;
+
+public class Pickup {
+
+    private Long id;
+
+    private Cliente client;
+
+    private String volume; // Documento, pacote ou caixa
+
+    private String collectionSource;
+
+    private String collectionDestination;
+
+    private String collectionSourceResponsible;
+
+    private String receiptDestinationResponsible;
+
+    private LocalDateTime collectionDate;
+
+    private LocalDateTime receiptDate;
+
+    private String status;
+
+    private String details;
+}
